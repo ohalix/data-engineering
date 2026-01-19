f7f10ac302b7745987e8c3c558a0c8d029cf40e3
⍥lamide#@![♟,♟] <87623048+ohalix@users.noreply.github.com>
2026-01-19 21:23:39 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+print('arguments:', sys.argv)
+month = sys.argv[1]
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df["month"] = month
+print(df.head())
+
+df.to_parquet(f'output_{month}.parquet')
+
+print(f'Hello from pipeline, month: {month}')
